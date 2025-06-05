ccc2b489e1ca239273f5025aaf8b9f54eea5904d
Raphael Benmimoune <raphaelbenmimoune1@gmail.com>
2025-06-05 14:29:01 +0200

    commit récent

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ export default function App() {
       <Header />
       <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-[var(--color-dark)] to-[var(--color-blood)]">
         <IntroSection />
-        
       </main>
       <AboutSection />
     </>
