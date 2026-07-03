94360d9309bd142662496e12dff2b362b65ffa9f
Raphael Benmimoune <raphaelbenmimoune1@gmail.com>
2026-07-03 19:03:35 +0200

    modification du logo

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -4,7 +4,7 @@ export default function IntroSection() {
   return (
     <section className="relative min-h-[60vh] flex flex-col items-center justify-center from-[var(--color-dark)] to-[var(--color-darker)] px-8">
       <motion.img
-        src="/assets/logo_officiel_clean.svg"
+        src="/assets/rb_logo.png"
         alt="Logo"
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
