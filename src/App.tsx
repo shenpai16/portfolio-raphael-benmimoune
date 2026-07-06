472c2372f362352f5028110dc24eae70797a1680
Raphael Benmimoune <raphaelbenmimoune1@gmail.com>
2026-07-06 16:38:20 +0200

    ajout du bandeau des cookies

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,35 @@ import Footer from "./layouts/Footer";
 
 import Home from "./components/ui/Home";
 import MentionsLegalesPages from "./pages/mentionsLegalesPages";
+import { useState } from "react";
 
 
 export default function App() {
+
+  const [cookiesAccepted, setCookiesAccepted] = useState(
+    localStorage.getItem("cookies") === "accepted"
+  );
+
+  const acceptCookies = () => {
+    localStorage.setItem("cookies", "accepted");
+    setCookiesAccepted(true);
+  }
+
+  
 return (
   <>
+  {!cookiesAccepted && (
+    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 flex justify-between items-center z-50">
+      <p className="text-sm">Ce site utilise des cookies pour améliorer votre expérience. En continuant à naviguer sur ce site, vous acceptez l'utilisation de cookies.</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={acceptCookies}
+      >
+        Accepter les cookies
+      </button>
+    </div>
+  )}
+
     <BrowserRouter>
       <Header />
 
